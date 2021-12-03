69c66fdca7dd7ac9a339a0cbdbba12275058ef52
zye7ert <edgarxot@gmail.com>
2021-12-03 00:52:10 -0500

    update header file

--- binary_trees.h
+++ binary_trees.h
@@ -74,7 +74,10 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
+/* ------ */
+binary_tree_t *binary_search_uncle(binary_tree_t *node);
+size_t balance_height(const binary_tree_t *tree);
+size_t recursive_height(const binary_tree_t *tree);
 
 
 
